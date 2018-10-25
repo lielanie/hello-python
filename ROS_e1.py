882ff659100b3312469b5a8d73999a24df356289
lielanie <44455578+lielanie@users.noreply.github.com>
2018-10-25 16:41:47 +0800

    Add files via upload

---
+++ ROS_e1.py
@@ -0,0 +1,12 @@
+data = {"name":"Lailanie Ros",
+"Spirit animal":" bird",
+"Want":"I love to travel",
+"Hobby":"I love to watch kdrama and read webtoon",
+"work":"Army"
+} 
+
+print("I am {}".format(data["name"]))
+print("My spirit animal {}".format(data["Spirit animal"]))
+print("Because {}" .format(data["Want"]))
+print("When not in school, I love to {}" .format(data["Hobby"]))
+print("I dream of being an {}" .format(data["work"]))
